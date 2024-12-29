3eeb8aba337249e0411e274701f17299b385774e
dinAlexDu <din.alex.du@gmail.com>
2024-12-29 11:36:58 +0000

    Fix: Use environment port for Azure compatibility

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use a porta fornecida pelo ambiente ou 3000 como fallback
 
 app.get('/', (req, res) => {
     res.send('Hello, Azure App Services!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
